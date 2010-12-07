66c67f4d06b64dffc73052f9169db3e04eefe9c6
colin <pollockcolin@gmail.com>
2010-12-06 20:58:09 -0800

    Realized the challenge stars at level 0, so each file's level number is moved down one.

--- level1.py
+++ level1.py
@@ -1,3 +1,21 @@
 #!/usr/bin/env python3
 
-print(2 ** 38)
+import string
+
+cipher_text = """g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq 
+ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq 
+rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu 
+ynnjw ml rfc spj.""" 
+
+# Make translation table from alphabet to alphabet + 2 positions
+frm = string.ascii_lowercase
+to = string.ascii_lowercase[2:] + string.ascii_lowercase[:2]
+trans_table = string.maketrans(frm, to)
+plain_text = string.translate(cipher_text, trans_table)
+print(plain_text)
+
+# Translate the url
+url = 'http://www.pythonchallenge.com/pc/def/.html'
+url_part = 'map'
+translated_part = string.translate(url_part, trans_table)
+print('http://www.pythonchallenge.com/pc/def/%s.html' % translated_part)
--- level2.py
+++ level2.py
@@ -1,21 +1,39 @@
 #!/usr/bin/env python3
 
-import string
+"""
+Find the "rarest characters" in a comment in the level's source.
+"""
 
-cipher_text = """g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq 
-ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq 
-rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu 
-ynnjw ml rfc spj.""" 
+import re
+import urllib.request
+import collections
+import operator
 
-# Make translation table from alphabet to alphabet + 2 positions
-frm = string.ascii_lowercase
-to = string.ascii_lowercase[2:] + string.ascii_lowercase[:2]
-trans_table = string.maketrans(frm, to)
-plain_text = string.translate(cipher_text, trans_table)
-print(plain_text)
+url = 'http://www.pythonchallenge.com/pc/def/ocr.html'
+html = urllib.request.urlopen(url).read().decode()
 
-# Translate the url
-url = 'http://www.pythonchallenge.com/pc/def/.html'
-url_part = 'map'
-translated_part = string.translate(url_part, trans_table)
-print('http://www.pythonchallenge.com/pc/def/%s.html' % translated_part)
+# Find the "mess" of characters.
+pat = re.compile(r'-->\s*<!--(.*?)-->', re.DOTALL)
+mess = pat.search(html).groups()[0]
+
+# Make a dict from characters in the mess to their frequencies.
+store = collections.defaultdict(int)
+for char in mess:
+    store[char] = store.get(char, 0) + 1
+
+# Convert the dictionary `store` to a list of (key, value) tuples and sort
+# the list by value.
+_sorted = sorted(store.items(), key=operator.itemgetter(1))
+
+# There are 8 characters that occur only once, while the rest occur at least
+# a thousand times. Print out those 8 characters.
+
+# There are 8 characters that occur only once, while the rest occur at least
+# a thousand times. Create a list of those 8 characters.
+good_chars = [tup[0] for tup in _sorted if tup[1] == 1]
+
+# The characters lost their order when put in a dictionary, so filter the mess,
+# keeping only characters in `good_chars`.
+answer = filter(lambda char: char in good_chars, mess)
+print(''.join(answer))
+
